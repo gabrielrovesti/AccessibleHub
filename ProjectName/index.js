7d58c760425f7b82872525e3a427ed3d95274252
Gabriel Rovesti <rovestigabriel@gmail.com>
2024-11-21 17:34:39 +0100

    Trying some more

---
+++ ProjectName/index.js
@@ -0,0 +1,4 @@
+import { registerRootComponent } from 'expo';
+import App from './app/index.tsx';  // or wherever your main App component is
+
+registerRootComponent(App);
---
+++ ProjectName/metro.config.js
@@ -0,0 +1,17 @@
+const {getDefaultConfig} = require('@react-native/metro-config');
+
+module.exports = (async () => {
+  const defaultConfig = await getDefaultConfig(__dirname);
+  return {
+    ...defaultConfig,
+    transformer: {
+      ...defaultConfig.transformer,
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+  };
+})();
